0f7d977d605b662bced9711c226eee6b2b991d90
Mavis Javier <89547162+mavis-javier@users.noreply.github.com>
2023-06-07 15:18:15 -0500

    Exercises 2.4-2.5

--- part2/courseinfo2/src/components/Course.js
+++ part2/courseinfo2/src/components/Course.js
@@ -2,14 +2,29 @@ import Header from './Header'
 import Content from './Content'
 import Total from './Total'
 
-const Course = ({course}) => {
-    console.log(course.name)
-    const sum = course.parts.reduce((total, current) => total + current.exercises, 0)
+const Course = ({ courses }) => {   
+    const courseSum = courses.map(
+      course => {
+        const test = course.parts.reduce(
+          (total, current) => total + current.exercises, 0)
+        console.log(test);
+        return (
+          course.parts.reduce(
+            (total, current) => total + current.exercises, 0)
+        )
+      }
+    )
+    console.log(courseSum);    
+    
     return (
         <>
-            <Header name={course.name}/>
-            <Content parts={course.parts}/>
-            <Total sum={sum} />
+           {courses.map( course => 
+            <div key={course.id}>
+              <Header name={course.name} />
+              <Content parts={course.parts} />
+              <Total parts={course.parts} />
+            </div>            
+           )}
         </>
     )
 }
--- part2/courseinfo2/src/components/Total.js
+++ part2/courseinfo2/src/components/Total.js
@@ -1,3 +1,11 @@
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const Total = ({ parts }) => {
+    console.log(parts);
+    const sum = parts.reduce(
+        (total, current) => total + current.exercises, 0)
+    return (
+        <p><strong>Total of {sum} exercises</strong></p>
+    )
+}
+
 
 export default Total;
