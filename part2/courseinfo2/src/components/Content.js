bfb8939ed00128ea866e1070a5fedbabac4fda7a
Mavis Javier <89547162+mavis-javier@users.noreply.github.com>
2023-06-07 11:04:18 -0500

    part 2 exercises 2.1-2.2

---
+++ part2/courseinfo2/src/components/Content.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    <Part
+      part={parts[0]} 
+    />
+    <Part
+      part={parts[1]} 
+    />
+    <Part
+      part={parts[2]} 
+    />      
+  </>
+
+  export default Content;
---
+++ part2/courseinfo2/src/components/Course.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './Header'
+import Content from './Content'
+import Total from './Total'
+
+const Course = (courses) => {
+  console.log(courses);
+  console.log(JSON.stringify(courses));
+}
+
+export default Course;
