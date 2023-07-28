d8a2450587998e535f329028f599dd49267c22db
Mavis Javier <89547162+mavis-javier@users.noreply.github.com>
2023-07-27 19:28:33 -0500

    Exercise 3.4
    user can provide URL for given ID

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -39,11 +39,24 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
 
+// 3.3: display # of contacts in phonebook and date and time request was made
 app.get('/info', (request, response) => {
     response.send(
         `<p>Phonebook has info for ${persons.length} people</p>
+        <br />        
         <p>${date}</p>`
     )
+})
+
+// 3.4: returns information of given ID in URL
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
   
 const PORT = 3001
